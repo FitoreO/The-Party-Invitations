52436f6338dd53888f32b5b5237bb8d71c09b10f
FitoreOsmanaj <fitore.osmanaj@puzzel.com>
2023-12-27 17:36:00 +0100

    feat: Enhance form functionality and add password field
    - Added form validation to ensure data integrity
    - Separated form functionality into its own file for better organization
    - Implemented temporary storage for form values
    - Added a new password field to the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,11 +2,47 @@ import {useState} from "react";
 
 const Form = () => {
 
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(0);
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        number: '',
+        email: '',
+        password: '',
+        participation: ""
+    });
+    const [savedFormData, setSavedFormData] = useState([]);
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormData((prevFormData) => ({...prevFormData, [name]: value}))
+    };
+
+    const handleRadioChange = (event) => {
+        const {value} = event.target;
+        setFormData((prevFormData) => {
+            return {...prevFormData, participation: value};
+        });
+    };
 
     const handleSubmit = (e) => {
+
+        if (!formData.firstName || !formData.lastName || !formData.number || !formData.email || !formData.password || !formData.participation) {
+            alert("Please fill in all required fields");
+            return;
+        }
+
         e.preventDefault();
         setCounter(counter + 1);
+        setSavedFormData((prevList) => [...prevList, formData]);
+        setFormData({
+            firstName: "",
+            lastName: "",
+            number: "",
+            email: "",
+            password: "",
+            participation: "",
+        })
     }
 
     return (
@@ -17,50 +53,78 @@ const Form = () => {
                     Will you be joining us?
                     <div className="participation">
                         <label>
-                            <input type="radio" value="Yes" name="participate"/>
+                            <input type="radio" name="participate" value="Yes"
+                                   checked={formData.participation === "Yes"}
+
+                                   onChange={handleRadioChange}
+                            />
                             Yes
                         </label>
                     </div>
                     <div className="participation">
                         <label>
-                            <input type="radio" value="No" name="participate"/>
+                            <input type="radio" name="participate" value="No" checked={formData.participation === "No"}
+
+                                   onChange={handleRadioChange}/>
                             No
                         </label>
                     </div>
                 </div>
                 <div className="fields-container">
                     <div className="fields-wrapper">
-                        <input className="input-fields" placeholder="First Name"/>
+                        <input className="input-fields" name="firstName" placeholder="First Name"
+                               onChange={handleChange}
+                               value={formData.firstName}/>
                         <label className="input-labels">
                             First Name
                         </label>
                     </div>
                     <div className="fields-wrapper">
-                        <input className="input-fields" placeholder="Last Name"/>
+                        <input className="input-fields" name="lastName" placeholder="Last Name"
+                               onChange={handleChange}
+                               value={formData.lastName}/>
                         <label className="input-labels">
                             Last Name
                         </label>
                     </div>
                     <div className="info-wrapper">
                         <div className="fields-wrapper">
-                            <input className="input-fields" placeholder="Phone Number"/>
+                            <input className="input-fields" placeholder="Phone Number"
+                                   onChange={handleChange} value={formData.number} name="number"/>
                             <label className="input-labels">
                                 Phone Number
                             </label>
                         </div>
                         <div className="fields-wrapper">
-                            <input className="input-fields" placeholder="Email"/>
+                            <input className="input-fields" type="email" placeholder="Email" name="email"
+                                   onChange={handleChange} value={formData.email}/>
                             <label className="input-labels">
                                 Email
                             </label>
                         </div>
                     </div>
+                    <div className="password-wrapper">
+                        <input className="input-fields" type="password" placeholder="password" name="password" value={formData.password}
+                               onChange={handleChange}/>
+                        <label className="input-labels">
+                            Password
+                        </label>
+                    </div>
                 </div>
                 <div className="button-wrapper">
-                    <button type="submit" className="submit-btn">Submit</button>
+                    <button type="submit" className="submit-btn" disabled={!formData.firstName || !formData.lastName ||!formData.participation || !formData.number || !formData.email || !formData.password}>Submit
+                    </button>
                 </div>
             </form>
             <p>Number of People that have signed up already:{counter}</p>
+            {savedFormData.map((formData, index) => (
+                <div key={index}>
+                    <p>Full Name: {formData.firstName} {formData.lastName}</p>
+                    <p>Will you participate: {formData.participation}</p>
+                    <p>Phone Number: {formData.number}</p>
+                    <p>Email: {formData.email}</p>
+                </div>))}
+
         </div>
     )
 }
