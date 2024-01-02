7d4d13bd47adaa9a7c53e48d4a0affc072db85e8
FitoreOsmanaj <fitore.osmanaj@puzzel.com>
2024-01-02 15:02:10 +0100

    cleaned up code not yet ready for production

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,12 @@
 'use client';
-import styles from '../styles/page.module.css'
+import styles from './page.module.css'
 import Form from "@/components/Form";
-import {useState} from "react";
-import LoginForm from "@/components/LoginForm";
 
 export default function Home() {
-    const [showForm, setShowForm] = useState(true);
-    const [showLoginForm, setShowLoginForm] = useState(false);
-    const [loginBtn, setLoginBtn] = useState(true);
-
-    const handleLoginBtn = () => {
-        setShowLoginForm(true);
-        setShowForm(false);
-        setLoginBtn(false);
-    }
 
     return (
         <main className={styles.main}>
-            {loginBtn && <button className="login-btn" onClick={handleLoginBtn}>Login</button>}
-            {showLoginForm && <LoginForm />}
-            {showForm && <Form/>}
+            <Form/>
         </main>
     )
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -9,22 +9,30 @@ const Form = () => {
         number: '',
         email: '',
         password: '',
+        participation: ""
     });
     const [savedFormData, setSavedFormData] = useState([]);
-    const [, setCorrectPassword] = useState(true);
 
     const handleChange = (event) => {
         const {name, value} = event.target;
         setFormData((prevFormData) => ({...prevFormData, [name]: value}))
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const handleRadioChange = (event) => {
+        const {value} = event.target;
+        setFormData((prevFormData) => {
+            return {...prevFormData, participation: value};
+        });
+    };
 
-        if (!formData.firstName || !formData.lastName || !formData.number || !formData.email || !formData.password) {
+    const handleSubmit = (e) => {
+
+        if (!formData.firstName || !formData.lastName || !formData.number || !formData.email || !formData.password || !formData.participation) {
             alert("Please fill in all required fields");
             return;
         }
+
+        e.preventDefault();
         setCounter(counter + 1);
         setSavedFormData((prevList) => [...prevList, formData]);
         setFormData({
@@ -33,22 +41,35 @@ const Form = () => {
             number: "",
             email: "",
             password: "",
+            participation: "",
         })
-        setCorrectPassword(true);
-
-        fetch('/api', {
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(formData)
-        }).then((data) => {
-            console.log("data response", data);
-        }).catch(error => console.log('error caught', error))
     }
 
     return (
         <div className="main-container">
             <p className="party-title">Welcome to the party of a lifetime</p>
             <form onSubmit={handleSubmit}>
+                <div className="participation-wrapper">
+                    Will you be joining us?
+                    <div className="participation">
+                        <label>
+                            <input type="radio" name="participate" value="Yes"
+                                   checked={formData.participation === "Yes"}
+
+                                   onChange={handleRadioChange}
+                            />
+                            Yes
+                        </label>
+                    </div>
+                    <div className="participation">
+                        <label>
+                            <input type="radio" name="participate" value="No" checked={formData.participation === "No"}
+
+                                   onChange={handleRadioChange}/>
+                            No
+                        </label>
+                    </div>
+                </div>
                 <div className="fields-container">
                     <div className="fields-wrapper">
                         <input className="input-fields" name="firstName" placeholder="First Name"
@@ -83,8 +104,7 @@ const Form = () => {
                         </div>
                     </div>
                     <div className="password-wrapper">
-                        <input className="input-fields" type="password" id="passwordInput" placeholder="password"
-                               name="password" value={formData.password}
+                        <input className="input-fields" type="password" placeholder="password" name="password" value={formData.password}
                                onChange={handleChange}/>
                         <label className="input-labels">
                             Password
@@ -92,8 +112,7 @@ const Form = () => {
                     </div>
                 </div>
                 <div className="button-wrapper">
-                    <button type="submit" className="submit-btn"
-                            disabled={!formData.firstName || !formData.lastName || !formData.number || !formData.email || !formData.password}>Submit
+                    <button type="submit" className="submit-btn" disabled={!formData.firstName || !formData.lastName ||!formData.participation || !formData.number || !formData.email || !formData.password}>Submit
                     </button>
                 </div>
             </form>
@@ -101,6 +120,7 @@ const Form = () => {
             {savedFormData.map((formData, index) => (
                 <div key={index}>
                     <p>Full Name: {formData.firstName} {formData.lastName}</p>
+                    <p>Will you participate: {formData.participation}</p>
                     <p>Phone Number: {formData.number}</p>
                     <p>Email: {formData.email}</p>
                 </div>))}
