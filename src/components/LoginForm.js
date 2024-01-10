589781410b93858261e5cab96497a57de786f14c
FitoreOsmanaj <fitore.osmanaj@puzzel.com>
2024-01-10 10:28:52 +0100

    Enhance LoginForm. On success login, show registered information from attendees

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,20 +1,15 @@
 import {useState} from "react";
 import '../styles/login-form.css';
-import Form from "@/components/Form";
-import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
 
 const LoginForm = () => {
-  //  const loginUserName = process.env.FORM_LOGIN_USERNAME;
-   // const loginPassword = process.env.FORM_LOGIN_PASSWORD;
-//const loginUserName = process.env.NEXT_PUBLIC_USERNAME;
-//const loginPassword = process.env.NEXT_PUBLIC_PASSWORD;
 
     const [loginData, setLoginData] = useState({
         username: '',
         password: ''
     });
-    const [error, setError] = useState('')
-
+    const [registeredData, setRegisteredData] = useState([]);
+    const [error, setError] = useState('');
+    const [showForm, setShowForm] = useState(true);
     const {username, password} = loginData;
 
     const changeHandler = (e) => {
@@ -23,49 +18,48 @@ const LoginForm = () => {
 
     const submitHandler = (e) => {
         e.preventDefault();
-        const enteredPassword = loginData.password;
-        const enteredUserName = loginData.username;
 
-        if (!enteredPassword || !enteredUserName) {
-            setError('Your login credentials are incorrect. Please try again.');
-        } else {
-            console.log('This is correct');
-        }
-
-
-           fetch('/api/admin', {
-               method: 'POST',
-               headers: {
-                   "Content-Type": "application/json"
-               },
-               body: JSON.stringify({
-                   username: enteredUserName,
-                   password: enteredPassword,
-               }),
-           }).then((res) => res.json())
-               .then((data) => {
-                   if (data.error) {
-                       setError(data.error)
-                   } else {
-                       setLoginData({
-                           username: '',
-                           password: ''
-                       })
-                   }
-               }).catch(error => console.log('error caught', error))
-
+        fetch('/api/admin', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(loginData),
+        }).then((res) => res.json())
+            .then((data) => {
+                if (data.error) {
+                    setError(data.error)
+                } else {
+                    setRegisteredData(data.res)
+                    setLoginData({
+                        username: '',
+                        password: ''
+                    })
+                    setShowForm(false)
+                    setError('');
+                }
+            }).catch(error => console.log('error caught', error))
     }
 
     return (
         <div>
-            <form onSubmit={submitHandler}>
+            {showForm && <form onSubmit={submitHandler}>
                 <input className="login-input-fields" type="text" name="username" placeholder="username"
                        value={username} onChange={changeHandler}/>
                 <input className="login-input-fields" type="password" name="password" value={password}
                        placeholder="password" onChange={changeHandler}/>
                 <button className="submit-login-btn" type="submit" name="submit">Log In</button>
-            </form>
-            {error}
+            </form>}
+            <ol>
+                {registeredData.map((data, index) => (
+                    <li key={index} className="registered-form-data">
+                        <p>Full Name: {data.firstName} {data.lastName}</p>
+                        <p>Phone Number: {data.number}</p>
+                        <p>Email: {data.email}</p>
+                    </li>
+                ))}
+            </ol>
+            {error && <div className="error-message">Invalid credentials</div>}
         </div>
     )
 }
